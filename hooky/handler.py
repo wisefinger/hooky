8ed4fb6994467d1b223546512e757a1aced181d4
Kris Cardoen <kris.cardoen@porphyrio.com>
2021-01-07 22:19:30 +0100

    added serverless offline

--- hooky/handler.py
+++ hooky/handler.py
@@ -4,12 +4,14 @@ import json
 def hello(event, context):
     body = {
         "message": "Go Go Serverless v1.0! "
-        #,"input": event
+        ,"input": event
+
     }
 
     response = {
         "statusCode": 200,
         "body": json.dumps(body)
+
     }
 
     return response
