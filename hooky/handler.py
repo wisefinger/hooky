4d44fea14da0733f5ae6ee41b9d04703fa5a8284
Kris Cardoen <kris.cardoen@porphyrio.com>
2021-01-10 21:51:48 +0100

    read parameters + validation

--- hooky/handler.py
+++ hooky/handler.py
@@ -2,11 +2,18 @@ import json
 
 
 def hello(event, context):
-    body = {
-        "message": "Go Go Serverless v1.0! "
-        ,"input": event
-
-    }
+    if 'queryStringParameters' in event and 'uid' in event['queryStringParameters']:
+        print("value found")
+        uid = (event["queryStringParameters"]['uid'])
+        body = {
+            "message": "GGGGo Go Serverless v1.0! ",
+            "uid": uid
+        }
+    else:
+        print("no valid value found")
+        body = {
+            "message": " no valid parameters found"
+        }
 
     response = {
         "statusCode": 200,
